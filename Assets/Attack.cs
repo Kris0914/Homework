03f6a52c2c9ef9a33bfdfd7ff5207f2fbc57b8e6
Kris0914 <ilovesky0904@gmail.com>
2025-04-11 15:23:47 +0900

    숙제

---
+++ Assets/Attack.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public int damage = 5;
+    private float attackRange = 15f;
+
+}
